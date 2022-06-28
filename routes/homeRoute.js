cdbbe0478299460c72865ad8707862e6b00ec12c
leandro-fz <lfz@mail.com>
2022-06-28 17:33:08 +0200

    home

---
+++ routes/homeRoute.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const routerHome = Router();
+
+//routerHome.post('/post', adminController.creaPost)
+//routerHome.post('/match/', adminController.creaUtente)
+//routerHome.get('/feed/', adminController.getAllPost)
+
+module.exports = routerHome;
--- routes/main-router.js
+++ routes/main-router.js
@@ -1,9 +1,11 @@
 const routerLogin = require('./auth')
 const routerCorsi = require('./courses')
 const routerAdmin = require('./adminRoute.js')
+const routerHome = require('./adminRoute.js')
 
 function ConnectRouter(app){
     app.use('/login', routerLogin)
+    app.use('/home', routerHome)
     app.use('/corsi', routerCorsi)
     app.use('/admin', routerAdmin )
 }
