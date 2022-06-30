ca0547747d38668261edc66acb6a5705a4ec60e2
leandro-fz <lfz@mail.com>
2022-06-30 09:10:30 +0200

    merging dei post nella home principale

--- routes/main-router.js
+++ routes/main-router.js
@@ -1,7 +1,6 @@
 const routerLogin = require('./auth')
 const routerCorsi = require('./coursesRouter')
 const routerAdmin = require('./adminRoute.js')
-
 const routerHome = require('./homeRoute.js')
 const routerProfile = require('./profileRoute.js')
 const controllaAutenticazione = require('../middlewares/checkAuth')
